ee577ffc8531d327bf1da0af9e2698d142b28022
Benoît Legat <benoit.legat@gmail.com>
2020-07-30 17:20:29 +0200

    Merge #11 with master

--- src/plotting.jl
+++ src/plotting.jl
@@ -11,25 +11,25 @@ const spatial = pyimport_conda("scipy.spatial", "scipy")
 FC(c, a) =  matplotlib.colors.colorConverter.to_rgba(c, alpha = a)
 
 function verts_rect(c, h)
-    return [c + [i*h[1], j*h[2]] for (i, j) in [(-1, -1), (-1, 1), (1, 1), (1, -1)]]
+    return (c .+ (-h[1], -h[2]), c .+ (-h[1], h[2]), c .+ (h[1], h[2]), c .+ (h[1], -h[2]))
 end
 
 ## =============================================================================
 # Cells
-function subspace!(ax, vars, sub_space;
-        fc = "red", fa = 0.5, ec = "black", ea = 1.0, ew = 1.5)
+function subset!(ax, vars, sub_set::AB.SubSet{N};
+        fc = "red", fa = 0.5, ec = "black", ea = 1.0, ew = 1.5) where N
     #---------------------------------------------------------------------------
-    grid_space = sub_space.grid_space
-    @assert length(vars) == 2 && grid_space.dim >= 2
+    grid_space = sub_set.grid_space
+    @assert length(vars) == 2 && N >= 2
     fca = FC(fc, fa)
     eca = FC(ec, ea)
 
-    pos_coll = (AB.get_pos_by_ref(grid_space, ref) for ref in AB.enumerate_subspace_ref(sub_space))
-    verts_vec = Vector{Vector{Float64}}[]
+    pos_iter = (AB.get_pos_by_ref(grid_space, ref) for ref in AB.enumerate_subset_ref(sub_set))
+    verts_vec = NTuple{4, Tuple{Float64, Float64}}[]
 
-    for pos in unique(x -> x[vars], pos_coll)
+    for pos in unique(x -> x[vars], pos_iter)
         c = AB.get_coords_by_pos(grid_space, pos)[vars]
-        push!(verts_vec, verts_rect(c, grid_space.h[vars]/2))
+        push!(verts_vec, verts_rect(c, grid_space.h[vars]./2))
     end
 
     poly_list = matplotlib.collections.PolyCollection(verts_vec)
@@ -45,8 +45,8 @@ function box!(ax, vars, lb, ub;
         fc = "green", fa = 0.5, ec = "black", ea = 1.0, ew = 1.5)
     #---------------------------------------------------------------------------
     @assert length(vars) == 2 && length(lb) == length(ub) >= 2
-    c = (lb + ub)/2
-    h = (ub - lb)/2
+    c = (lb .+ ub)./2
+    h = (ub .- lb)./2
     poly_list = matplotlib.collections.PolyCollection([verts_rect(c[vars], h[vars])])
     fca = FC(fc, fa)
     eca = FC(ec, ea)
@@ -58,22 +58,22 @@ end
 
 ## =============================================================================
 # Trajectory open loop
-function trajectory_open_loop!(ax, vars, cont_sys, x0, u, nstep;
-        lc = "red", lw = 1.5, mc = "black", ms = 5.0, nsub = 5)
+function trajectory_open_loop!(ax, vars, cont_sys, x0::NTuple{N, Float64}, u, nstep;
+        lc = "red", lw = 1.5, mc = "black", ms = 5.0, nsub = 5) where N
     #---------------------------------------------------------------------------
-    @assert length(vars) == 2 && length(x0) >= 2
+    @assert length(vars) == 2 && N >= 2
     tstep = cont_sys.tstep/nsub
     Nstep = nstep*nsub + 1
     X1_vec = Vector{Float64}(undef, Nstep)
     X2_vec = Vector{Float64}(undef, Nstep)
     X1_vec[1] = x0[vars[1]]
     X2_vec[1] = x0[vars[2]]
-    x0 = copy(x0)
+    x = x0
 
     for i = 2:Nstep
-        cont_sys.sys_map!(x0, u, tstep)
-        X1_vec[i] = x0[vars[1]]
-        X2_vec[i] = x0[vars[2]]
+        x = cont_sys.sys_map(x, u, tstep)
+        X1_vec[i] = x[vars[1]]
+        X2_vec[i] = x[vars[2]]
     end
 
     idx = 1:nsub:Nstep
@@ -83,23 +83,23 @@ end
 
 ## =============================================================================
 # Images
-function cell_image!(ax, vars, X_sub, U_sub, cont_sys;
-        nsub = fill(5, X_sub.grid_space.dim), fc = "blue", fa = 0.5, ec = "darkblue", ea = 1.0, ew = 1.5)
+function cell_image!(ax, vars, X_sub, U_sub, cont_sys::AB.ControlSystem{N};
+        nsub = fill(5, N), fc = "blue", fa = 0.5, ec = "darkblue", ea = 1.0, ew = 1.5) where N
     #---------------------------------------------------------------------------
-    @assert length(vars) == 2 && X_sub.grid_space.dim >= 2
+    @assert length(vars) == 2 && N >= 2
     fca = FC(fc, fa)
     eca = FC(ec, ea)
-    verts_list = Vector{Vector{Float64}}[]
+    verts_list = Vector{Tuple{Float64, Float64}}[]
     ns = nsub .- 1
 
-    for x_ref in AB.enumerate_subspace_ref(X_sub), u_ref in AB.enumerate_subspace_ref(U_sub)
+    for x_ref in AB.enumerate_subset_ref(X_sub), u_ref in AB.enumerate_subset_ref(U_sub)
         x = AB.get_coords_by_ref(X_sub.grid_space, x_ref)
         u = AB.get_coords_by_ref(U_sub.grid_space, u_ref)
-        pos_ = ((0:ns[i])./ns[i] .- 0.5 for i = 1:length(ns))
-        x_coll = (x + pos.*X_sub.grid_space.h for pos in Iterators.product(pos_...))
-        sys_map = x -> (cont_sys.sys_map!(x, u, cont_sys.tstep); return x)
-        Fx_coll = (sys_map(x) for x in x_coll)
-        Fxproj_vec = [Fx[vars] for Fx in Fx_coll][:]
+        subpos_axes = ((0:ns[i])./ns[i] .- 0.5 for i = 1:length(ns))
+        subpos_iter = Iterators.product(subpos_axes...)
+        x_iter = (x .+ subpos.*X_sub.grid_space.h for subpos in subpos_iter)
+        Fx_iter = (cont_sys.sys_map(x, u, cont_sys.tstep) for x in x_iter)
+        Fxproj_vec = [Fx[vars] for Fx in Fx_iter][:]
         hull = spatial.ConvexHull([Fx[i] for Fx in Fxproj_vec, i in 1:2])
         push!(verts_list, Fxproj_vec[hull.vertices .+ 1])
     end
@@ -113,22 +113,22 @@ end
 
 ## =============================================================================
 # Outer-approximation
-function cell_approx!(ax, vars, X_sub, U_sub, cont_sys;
-        fc = "yellow", fa = 0.5, ec = "gold", ea = 1.0, ew = 0.5)
+function cell_approx!(ax, vars, X_sub, U_sub, cont_sys::AB.ControlSystem{N};
+        fc = "yellow", fa = 0.5, ec = "gold", ea = 1.0, ew = 0.5) where N
     #---------------------------------------------------------------------------
-    @assert length(vars) == 2 && X_sub.grid_space.dim >= 2
+    @assert length(vars) == 2 && N >= 2
     fca = FC(fc, fa)
     eca = FC(ec, ea)
-    verts_list = Vector{Vector{Float64}}[]
+    verts_list = NTuple{4, Tuple{Float64, Float64}}[]
 
-    for x_ref in AB.enumerate_subspace_ref(X_sub), u_ref in AB.enumerate_subspace_ref(U_sub)
+    for x_ref in AB.enumerate_subset_ref(X_sub), u_ref in AB.enumerate_subset_ref(U_sub)
         x = AB.get_coords_by_ref(X_sub.grid_space, x_ref)
         u = AB.get_coords_by_ref(U_sub.grid_space, u_ref)
-        cont_sys.sys_map!(x, u, cont_sys.tstep)
-        r = X_sub.grid_space.h/2 + cont_sys.meas_noise
-        cont_sys.bound_map!(r, u, cont_sys.tstep)
-        r += cont_sys.meas_noise
-        push!(verts_list, verts_rect(x[vars], r[vars]))
+        Fx = cont_sys.sys_map(x, u, cont_sys.tstep)
+        r = X_sub.grid_space.h./2 .+ cont_sys.meas_noise
+        Fr = cont_sys.bound_map(r, u, cont_sys.tstep)
+        Fr = Fr .+ cont_sys.meas_noise
+        push!(verts_list, verts_rect(Fx[vars], Fr[vars]))
     end
 
     poly_list = matplotlib.collections.PolyCollection(verts_list)
@@ -140,27 +140,30 @@ end
 
 ## =============================================================================
 # Trajectory closed loop
-function trajectory_closed_loop!(ax, vars, cont_sys, trans_map, x0, nstep;
+function trajectory_closed_loop!(ax, vars, cont_sys, sym_model, x0, nstep;
         lc = "red", lw = 1.5, mc = "black", ms = 5.0, nsub = 5)
     #---------------------------------------------------------------------------
-    X_grid = trans_map.X_grid
-    x0 = copy(x0)
+    X_grid = sym_model.X_grid
+    U_sub = AB.NewSubSet(sym_model.U_grid)
+    Y_sub = AB.NewSubSet(sym_model.Y_grid)
     for i = 1:nstep
+        AB.remove_from_subset_all!(U_sub)
+        AB.remove_from_subset_all!(Y_sub)
         x_ref = AB.get_ref_by_coords(X_grid, x0)
         if x_ref === X_grid.overflow_ref
             @warn("Trajectory out of domain")
             return
         end
-        uy_ref_coll = AB.get_transition_image(trans_map, x_ref)
-        if isempty(uy_ref_coll)
+        if ~AB.is_xref_controllable(sym_model, x_ref)
             @warn("Uncontrollable state")
             return
         end
-        u_ref = iterate(uy_ref_coll)[1][1]
-        u = AB.get_coords_by_ref(trans_map.U_grid, u_ref)
-        trajectory_open_loop!(
-            ax, vars, cont_sys, x0, u, 1, lc = lc, lw = lw, mc = mc, ms = ms, nsub = nsub)
-        cont_sys.sys_map!(x0, u, cont_sys.tstep)
+        AB.add_inputs_images_by_xref!(U_sub, Y_sub, sym_model, x_ref)
+        u_ref = iterate(AB.enumerate_subset_ref(U_sub))[1]
+        u = AB.get_coords_by_ref(sym_model.U_grid, u_ref)
+        AB.trajectory_open_loop!(ax, vars, cont_sys, x0, u, 1,
+            lc = lc, lw = lw, mc = mc, ms = ms, nsub = nsub)
+        x0 = cont_sys.sys_map(x0, u, cont_sys.tstep)
     end
 end
 
--- test/test_controlsystem.jl
+++ test/test_controlsystem.jl
@@ -10,32 +10,32 @@ sleep(0.1) # used for good printing
 println("Started test")
 
 @testset "Control" begin
-lb = [0.0, 0.0]
-ub = [10.0, 11.0]
-x0 = [0.0, 0.0]
-h = [1.0, 2.0]
+lb = (0.0, 0.0)
+ub = (10.0, 11.0)
+x0 = (0.0, 0.0)
+h = (1.0, 2.0)
 X_grid = AB.NewGridSpaceHash(x0, h)
 @test AB.get_gridspace_size(X_grid) == 0
 AB.add_to_gridspace!(X_grid, AB.HyperRectangle(lb, ub), AB.OUTER)
 @test AB.get_gridspace_size(X_grid) == 77
 
-lb = [-1.0]
-ub = [1.0]
-u0 = [0.0]
-h = [0.5]
+lb = (-1.0,)
+ub = (1.0,)
+u0 = (0.0,)
+h = (0.5,)
 U_grid = AB.NewGridSpaceHash(u0, h)
 AB.add_to_gridspace!(U_grid, AB.HyperRectangle(lb, ub), AB.OUTER)
 @test AB.get_gridspace_size(U_grid) == 5
 
-tstep = 0.5
+tstep = 1.0
 n_sys = 3
 n_bound = 3
-# F_sys(x, u) = [1.0-cos(x[2]), -x[1] + u[1]]
-# L_bound(u) = [0.0 1.0; 1.0 0.0]
-F_sys(x, u) = [u[1], -cos(x[1])]
-L_bound(u) = [0.0 0.0; 1.0 0.0]
-sys_noise = [1.0, 1.0]*0.01
-meas_noise = [1.0, 1.0]*0.01
+# F_sys(x, u) = (1.0-cos(x(2)), -x(1) + u(1)
+# L_bound(u) = (0.0 1.0; 1.0 0.0)
+F_sys(x, u) = (u[1], -cos(x[1]))
+L_bound(r, u) = (0.0, r[1])
+sys_noise = (1.0, 1.0).*0.01
+meas_noise = (1.0, 1.0).*0.01
 
 cont_sys = AB.NewControlSystemRK4(tstep, F_sys, L_bound, sys_noise, meas_noise, n_sys, n_bound)
 
@@ -46,25 +46,25 @@ cont_sys = AB.NewControlSystemRK4(tstep, F_sys, L_bound, sys_noise, meas_noise, 
     ax = fig.gca()
 end
 
-x_pos = [1, 1]
-u_pos = [1]
+x_pos = (1, 1)
+u_pos = (1,)
 x = AB.get_coords_by_pos(X_grid, x_pos)
 u = AB.get_coords_by_pos(U_grid, u_pos)
-X_simple = AB.NewSubSpace(X_grid)
-AB.add_to_subspace_by_pos!(X_simple, x_pos)
-@test AB.get_subspace_size(X_simple) == 1
-U_simple = AB.NewSubSpace(U_grid)
-AB.add_to_subspace_by_pos!(U_simple, u_pos)
-@test AB.get_subspace_size(U_simple) == 1
+X_simple = AB.NewSubSet(X_grid)
+AB.add_to_subset_by_pos!(X_simple, x_pos)
+@test AB.get_subset_size(X_simple) == 1
+U_simple = AB.NewSubSet(U_grid)
+AB.add_to_subset_by_pos!(U_simple, u_pos)
+@test AB.get_subset_size(U_simple) == 1
 
 @static if get(ENV, "TRAVIS", "false") == "false"
-    Plot.subspace!(ax, 1:2, X_simple)
+    Plot.subset!(ax, 1:2, X_simple)
     Plot.trajectory_open_loop!(ax, 1:2, cont_sys, x, u, 50)
     Plot.cell_image!(ax, 1:2, X_simple, U_simple, cont_sys)
     Plot.cell_approx!(ax, 1:2, X_simple, U_simple, cont_sys)
 
-    ax.set_xlim([-10.0, 10.0])
-    ax.set_ylim([-10.0, 10.0])
+    ax.set_xlim((-10.0, 10.0))
+    ax.set_ylim((-10.0, 10.0))
 end
 end
 
