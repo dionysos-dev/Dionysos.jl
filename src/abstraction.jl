b83376b80d1907a6c6872d32f67620d19046aa4b
Benoît Legat <benoit.legat@gmail.com>
2020-07-31 18:23:09 +0200

    Type stable data structure

--- src/abstraction.jl
+++ src/abstraction.jl
@@ -21,20 +21,20 @@ struct SubSetHash{N} <: SubSet{N}
     elems::Set{UInt64}
 end
 
-struct ControlSystem{N}
+struct ControlSystem{N, Fsys<:Function, Fbound<:Function}
     tstep::Float64
     sys_noise::NTuple{N, Float64}
     meas_noise::NTuple{N, Float64}
-    sys_map::Function
-    bound_map::Function
+    sys_map::Fsys
+    bound_map::Fbound
 end
 
 abstract type SymbolicModel end
 
-mutable struct SymbolicModelHash <: SymbolicModel
-    X_grid::GridSpaceHash
-    U_grid::GridSpaceHash
-    Y_grid::GridSpaceHash
+mutable struct SymbolicModelHash{N1, N2, N3} <: SymbolicModel
+    X_grid::GridSpaceHash{N1}
+    U_grid::GridSpaceHash{N2}
+    Y_grid::GridSpaceHash{N3}
     elems::Vector{Tuple{UInt64, UInt64, UInt64}}
     issorted::Bool
     isunique::Bool
