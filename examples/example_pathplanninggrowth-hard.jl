5f5e99c885d76ee9ddf432b92f33285b5c91ed8e
guberger <guillaume.berger@uclouvain.be>
2020-08-10 17:20:55 +0200

    progressmeter

--- examples/example_pathplanninggrowth-hard.jl
+++ examples/example_pathplanninggrowth-hard.jl
@@ -1,2 +1,2 @@
 include("example_pathplanning.jl")
-PathPlanning.path_planning(10.0, nsteps = 1000, approx_mode = "growth", verbose = true)
+PathPlanning.path_planning(10.0, nsteps = 1000, approx_mode = "growth")
--- src/controller.jl
+++ src/controller.jl
@@ -52,7 +52,7 @@ function _compute_npoststable(npoststable, autom)
 end
 
 # Assumes contr is "empty"
-function compute_controller_reach!(contr, autom, initlist, targetlist; verbose = false)
+function compute_controller_reach!(contr, autom, initlist, targetlist)
     println("compute_controller_reach! started")
     nstates = autom.nstates
     nsymbols = autom.nsymbols
@@ -73,13 +73,9 @@ function compute_controller_reach!(contr, autom, initlist, targetlist; verbose =
     soursymblist = Tuple{Int,Int}[]
 
     # Commented because it changes the number of allocations
-    if verbose
-        prog = ProgressUnknown("# iterations computing controller:")
-    end
+    # prog = ProgressUnknown("# iterations computing controller:")
     while !isempty(initset)
-        if verbose
-            ProgressMeter.next!(prog)
-        end
+        # ProgressMeter.next!(prog)
         for source in targetset
             for symbol = 1:nsymbols
                 npoststable[source, symbol] = 0
@@ -107,9 +103,7 @@ function compute_controller_reach!(contr, autom, initlist, targetlist; verbose =
         end
         if isempty(nexttargetset)
             println("\ncompute_controller_reach! terminated without covering init set")
-            if verbose
-                ProgressMeter.finish!(prog)
-            end
+            # ProgressMeter.finish!(prog)
             return
         end
         setdiff!(initset, nexttargetset)
@@ -117,9 +111,7 @@ function compute_controller_reach!(contr, autom, initlist, targetlist; verbose =
         empty!(nexttargetset)
         # unique!(targetlist)
     end
-    if verbose
-        ProgressMeter.finish!(prog)
-    end
+    # ProgressMeter.finish!(prog)
     println("\ncompute_controller_reach! terminated with success")
 end
 
