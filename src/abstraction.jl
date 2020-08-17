dda8cc0d55d64cf6bb1ae2366abff40164f5eba1
guberger <guillaume.berger@uclouvain.be>
2020-08-17 14:32:04 +0200

    New hash function

--- src/abstraction.jl
+++ src/abstraction.jl
@@ -3,8 +3,17 @@ module Abstraction
 using LinearAlgebra
 using ProgressMeter
 using StaticArrays
+using Base.Cartesian
 
 @enum INCL_MODE INNER OUTER
+
+Base.hash(x::Tuple{}, h::UInt) = h + Base.tuplehash_seed
+@generated function Base.hash(x::NTuple{N}, h::UInt) where N
+    quote
+        h += Base.tuplehash_seed
+        @nexprs $N i -> h = hash(x[$N-i+1], h)
+    end
+end
 
 include("rectangle.jl")
 include("polyhedron.jl")
