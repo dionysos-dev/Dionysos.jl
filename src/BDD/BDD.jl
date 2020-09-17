08a0ff4aedfbc9e9408c72f20c8f05efb7c9362f
Guillaume Berger <guillaume.berger@uclouvain.be>
2020-09-17 23:38:38 +0200

    IntSets with VariablesClusters

--- src/BDD/BDD.jl
+++ src/BDD/BDD.jl
@@ -2,7 +2,7 @@ module BDD
 
 using CUDD
 
-# Helper functions
+## Helper functions
 
 _One(dd::Ptr{Manager}) = CUDD.Cudd_ReadOne(dd)
 _Zero(dd::Ptr{Manager}) = CUDD.Cudd_ReadLogicZero(dd)
@@ -43,150 +43,61 @@ function _Cube(dd::Ptr{Manager}, indices::Vector{Cint}, phases::Vector{Cint})
     return cube
 end
 
-struct BDDManager
+## BDD + vector to feed values without allocating too much
+mutable struct BDDManager
     dd::Ptr{Manager}
     values::Vector{Cint}
 end
 
 function BDDManager()
-    dd = CUDD.Cudd_Init()
-    values = Cint[]
-    mng = BDDManager(dd, values)
+    mng = BDDManager(CUDD.Cudd_Init(), Cint[])
     finalizer(mng -> CUDD.Cudd_Quit(mng.dd), mng)
     return mng
 end
 
-#---
-
-@inline _bit(x::T) where T<:Integer = iszero(x & T(1)) ? Cint(0) : Cint(1)
-
-include("cartesianproduct")
-include("intset.jl")
-include("inttupleset.jl")
-
-Base.IteratorSize(::S) where {S<:Union{IntSet,IntTupleSet}} = Base.SizeUnknown()
-Base.isempty(set::S) where {S<:Union{IntSet,IntTupleSet}} = set.root === _Zero(set.mng)
-Base.empty!(set::IntSet) = _empty!(set)
-Base.empty!(set::IntTupleSet) = _empty!(set)
-function _empty!(set)
-    _Deref(set.mng, set.root)
-    set.root = _Zero(set.mng); _Ref(set.root)
-    return set
+## A set a BDD variables depending on a manager.
+# The goal is that BDD functions defined with a given Cluster as support are
+# automatically updated when a variable is added to the support.
+# The updating rule for the moment is:
+# {new f}(x_1,...,x_n,x_{n+1}) = f(x_1,...,x_n) ∧ ¬x_{n+1}
+# TODO: Add other updating rule depending on the type of the function (create new
+# types for that), or either on the function itself (via an updating function
+# stored in struct along with the boolean function).
+mutable struct VariablesCluster
+    mng::BDDManager
+    nvars::Int
+    indices::Vector{Cint}
+    phases::Vector{Cint}
+    Rroots::Vector{Ref{Ptr{Node}}}
 end
 
-_dims2string(nbits) = length(nbits) === 1 ? nbits[1] : Base.dims2string(nbits)
-Base.show(io::IO, set::S) where {S<:Union{IntSet,IntTupleSet}} = Base.summary(io, set)
-function Base.summary(io::IO, set::S) where {S<:Union{IntSet,IntTupleSet}}
-    nbits = length.(set.indices_)
-    Base.showarg(io, set, true)
-    print(io, " with ", _dims2string(nbits), " bit")
-    isone(prod(nbits)) || print(io, "s")
+function VariablesCluster(mng::BDDManager)
+    return VariablesCluster(mng, 0, Cint[], Cint[], Ref{Ptr{Node}}[])
 end
 
-#---
-function _compute_phases!(mng, phases, indices, auxphases, auxindices, e)
-    for r in eachindex(indices)
-        phases[r] = _bit(e)
-        e >>>= 1
-    end
-    nvars = _Size(mng)
-    while e > 0
-        _IthVar(mng, nvars)
-        push!(phases, _bit(e))
-        push!(indices, nvars)
-        push!(auxphases, Cint(0))
-        push!(auxindices, nvars)
-        nvars += Cint(1)
-        e >>>= 1
+function add_var!(vc::VariablesCluster)
+    dd = vc.mng.dd
+    nvars = _Size(dd)
+    newvar = _IthVar(dd, nvars)
+    resize!(vc.mng.values, nvars + 1)
+    push!(vc.indices, nvars)
+    vc.nvars += 1
+    resize!(vc.phases, vc.nvars)
+    for Rroot in vc.Rroots
+        not_var = _NOT(dd, newvar); _Ref(not_var)
+        tmp = _AND(dd, Rroot[], not_var); _Ref(tmp)
+        _Deref(dd, not_var); _Deref(dd, Rroot[])
+        Rroot[] = tmp
     end
 end
 
-Base.push!(set::IntSet{T}, x::T) where T = _push!(set, x)
-Base.push!(set::IntTupleSet{N,T}, x::NTuple{N,T}) where {N,T} = _push!(set, x)
-function _push!(set, x)
-    _phases1!(set, x)
-    mng = set.mng
-    indices_ = set.indices_
-    phases_ = set.phases1_
-    auxindices_ = set.auxindices_
-    auxphases_ = set.auxphases_
-    root = set.root # root is already referenced
-    for i in eachindex(indices_)
-        cube = _Cube(mng, auxindices_[i], auxphases_[i]); _Ref(cube)
-        tmp = _AND(mng, root, cube); _Ref(tmp)
-        _Deref(mng, cube); _Deref(mng, root)
-        root = tmp
+function set_values!(vc)
+    for (idx, phase) in zip(vc.indices, vc.phases)
+        vc.mng.values[idx+1] = phase
     end
-    # At this point root is referenced
-    elem = _One(mng); _Ref(elem)
-    for i in eachindex(indices_)
-        cube = _Cube(mng, indices_[i], phases_[i]); _Ref(cube)
-        tmp = _AND(mng, elem, cube); _Ref(tmp)
-        _Deref(mng, cube); _Deref(mng, elem)
-        elem = tmp
-    end
-    # At this point elem is referenced
-    tmp = _OR(mng, root, elem); _Ref(tmp)
-    _Deref(mng, root); _Deref(mng, elem)
-    set.root = tmp
-    return set
 end
 
-#---
-# Returns true if e can be represented with bits in phases
-function _compute_phases_trunc!(phases, e)
-    for r in eachindex(phases)
-        phases[r] = _bit(e)
-        e >>>= 1
-    end
-    return iszero(e)
-end
-
-Base.in(x::T, set::IntSet{T}) where {N,T<:Integer} = _in(x, set)
-Base.in(x, ::IntSet) = false
-Base.in(x::NTuple{N,T}, set::IntTupleSet{N,T}) where {N,T<:Integer} = _in(x, set)
-Base.in(x, ::IntTupleSet) = false
-function _fill_values(values, phases_, indices_)
-    for i in eachindex(indices_)
-        indices = indices_[i]
-        phases = phases_[i]
-        for r in eachindex(indices)
-            values[indices[r]+1] = phases[r]
-        end
-    end
-end
-function _eval_phases(set)
-    resize!(set.values, _Size(set.mng))
-    _fill_values(set.values, set.phases1_, set.indices_)
-    return _Eval(set.mng, set.root, set.values)
-end
-function _in(x, set)
-    _phases1_trunc!(set, x) !== 0 && return false
-    return _eval_phases(set)
-end
-
-Base.delete!(set::IntSet, x) = _delete!(set, x)
-Base.delete!(set::IntTupleSet, x) = _delete!(set, x)
-function _delete!(set, x)
-    # ∈ updates `set.phases1`
-    x ∈ set || return set
-    mng = set.mng
-    indices_ = set.indices_
-    phases_ = set.phases1_
-    elem = _One(mng); _Ref(elem)
-    for i in eachindex(indices_)
-        cube = _Cube(mng, indices_[i], phases_[i]); _Ref(cube)
-        tmp = _AND(mng, elem, cube); _Ref(tmp)
-        _Deref(mng, cube); _Deref(mng, elem)
-        elem = tmp
-    end
-    # At this point elem is referenced
-    not_elem = _NOT(mng, elem); _Ref(not_elem)
-    _Deref(mng, elem)
-    tmp = _AND(mng, set.root, not_elem); _Ref(tmp)
-    _Deref(mng, set.root); _Deref(mng, not_elem)
-    set.root = tmp
-    return set
-end
+## -
+include("integerset.jl")
 
 end # module
---
+++ src/BDD/integerset.jl
@@ -0,0 +1,221 @@
+#
+## IntSet
+"""
+    mutable struct IntSet <: AbstractSet{<:Integer}
+
+Same as `Base.Set{<:Integer}` but with `CUDD`.
+"""
+mutable struct IntSet{T<:Integer} <: AbstractSet{T}
+    mng::BDDManager
+    vcs::Tuple{VariablesCluster}
+    Rroot::Ref{Ptr{Node}}
+end
+
+function IntSet{T}(vc::VariablesCluster) where T
+    mng = vc.mng
+    root = _Zero(mng.dd); _Ref(root)
+    Rroot = Ref(root)
+    push!(vc.Rroots, Rroot)
+    return IntSet{T}(mng, tuple(vc), Rroot)
+end
+IntSet(vc::VariablesCluster) = IntSet{Int}(vcs)
+
+function IntSet{T}() where T
+    mng = BDDManager()
+    vc = VariablesCluster(mng)
+    set = IntSet{T}(vc)
+    finalizer(set -> finalize(set.mng), set)
+    return set
+end
+IntSet() = IntSet{Int}()
+
+Base.eltype(::Type{IntSet{T}}) where T = T
+Base.empty(::IntSet, ::Type{T}=Int) where T = IntSet{T}()
+Base.emptymutable(::IntSet, ::Type{T}=Int) where T = IntSet{T}()
+
+_phases!(set::IntSet, x) = _phases_simple!(set.vcs[1], x)
+
+# Return 0 if there are enough bits to represent x.
+# Returning a bool would have been more straighforward but this is to be consistent
+# with IntTupleSet.
+function _phases_trunc!(set::IntSet, x)
+    not_trunc = _phases_trunc_simple!(set.vcs[1], x)
+    return not_trunc ? 0 : 1
+end
+
+function Base.iterate(set::IntSet{T}, state::T=zero(T)) where T
+    _phases_trunc!(set, state) > 0 && return nothing
+    _eval_values_set(set) && return (state, state + 1)
+    return iterate(set, state + 1)
+end
+
+## IntTupleSet
+"""
+    mutable struct IntTupleSet <: AbstractSet{NTuple{N,<:Integer} where N}
+
+Same as `Base.Set{NTuple{N,<:Integer} where N}` but with `CUDD`.
+"""
+mutable struct IntTupleSet{N,T<:Integer} <: AbstractSet{NTuple{N,T}}
+    mng::BDDManager
+    vcs::NTuple{N,VariablesCluster}
+    Rroot::Ref{Ptr{Node}}
+end
+
+function IntTupleSet{N,T}(vcs::NTuple{N,VariablesCluster}) where {N,T}
+    mng = vcs[1].mng
+    @assert all(vc -> vc.mng === mng, vcs)
+    root = _Zero(mng.dd); _Ref(root)
+    Rroot = Ref(root)
+    for vc in vcs
+        push!(vc.Rroots, Rroot)
+    end
+    return IntTupleSet{N,T}(mng, vcs, Rroot)
+end
+IntTupleSet{N}(vcs::NTuple{N,VariablesCluster}) where N = IntTupleSet{N,Int}(vcs)
+
+function IntTupleSet{N,T}() where {N,T}
+    mng = BDDManager()
+    vcs = ntuple(i -> VariablesCluster(mng), Val(N))
+    set = IntTupleSet{N,T}(vcs)
+    finalizer(set -> finalize(set.mng), set)
+    return set
+end
+IntTupleSet{N}() where N = IntTupleSet{N,Int}()
+
+Base.eltype(::Type{IntTupleSet{N,T}}) where {N,T} = NTuple{N,T}
+Base.empty(::IntTupleSet{N}, ::Type{T}=Int) where {N,T} = IntTupleSet{N,T}()
+Base.emptymutable(::IntTupleSet{N}, ::Type{T}=Int) where {N,T} = IntTupleSet{N,T}()
+
+function _phases!(set::IntTupleSet, x)
+    for (vc, e) in zip(set.vcs, x)
+        _phases_simple!(vc, e)
+    end
+end
+
+# Returns the first i for which there is not enough bits to represent x[i];
+# Returns 0 if there is no such i.
+function _phases_trunc!(set::IntTupleSet, x)
+    i = 1
+    for (vc, e) in zip(set.vcs, x)
+        !_phases_trunc_simple!(vc, e) && return i
+        i += 1
+    end
+    return 0
+end
+
+# TODO: Can we improve this?
+@inline _incr_(e::T, i, j) where T = i == j ? zero(T) : (i == j + 1 ? e + one(T) : e)
+function _increment(x::NTuple{N}, j) where N
+    return ntuple(i -> _incr_(x[i], i, j), Val(N))
+end
+
+Base.iterate(set::IntTupleSet{N,T}) where {N,T} = iterate(set, ntuple(i -> zero(T), Val(N)))
+function Base.iterate(set::IntTupleSet{N}, state::NTuple{N}) where N
+    I = _phases_trunc!(set, state)
+    I === N && return nothing
+    I === 0 && _eval_values_set(set) && return (state, _increment(state, 0))
+    return iterate(set, _increment(state, I))
+end
+
+## High-level functions
+Base.IteratorSize(::S) where {S<:Union{IntSet,IntTupleSet}} = Base.SizeUnknown()
+Base.isempty(set::S) where {S<:Union{IntSet,IntTupleSet}} = set.Rroot[] === _Zero(set.mng.dd)
+Base.empty!(set::IntSet) = _empty!(set)
+Base.empty!(set::IntTupleSet) = _empty!(set)
+function _empty!(set)
+    _Deref(set.mng.dd, set.Rroot[])
+    set.Rroot[] = _Zero(set.mng.dd); _Ref(set.Rroot[])
+    return set
+end
+
+_dims2string(nbits) = length(nbits) === 1 ? nbits[1] : Base.dims2string(nbits)
+Base.show(io::IO, set::S) where {S<:Union{IntSet,IntTupleSet}} = Base.summary(io, set)
+function Base.summary(io::IO, set::S) where {S<:Union{IntSet,IntTupleSet}}
+    nbits = map(vc -> vc.nvars, set.vcs)
+    Base.showarg(io, set, true)
+    print(io, " with ", _dims2string(nbits), " bit")
+    isone(prod(nbits)) || print(io, "s")
+end
+
+_bit(x::T) where T<:Integer = iszero(x & T(1)) ? Cint(0) : Cint(1)
+
+# Adds new variables to vc and updates the phases of vc so that they contain the
+# full bit representation of e
+function _phases_simple!(vc, e)
+    for r in 1:vc.nvars
+        vc.phases[r] = _bit(e)
+        e >>>= 1
+    end
+    while e > 0
+        add_var!(vc)
+        vc.phases[vc.nvars] = _bit(e)
+        e >>>= 1
+    end
+end
+
+Base.push!(set::IntSet{T}, x::T) where T = _push!(set, x)
+Base.push!(set::IntTupleSet{N,T}, x::NTuple{N,T}) where {N,T} = _push!(set, x)
+function _push!(set, x)
+    _phases!(set, x)
+    # Here the phases of vcs[i] contain the full bit rep of x[i]
+    dd = set.mng.dd
+    elem = _One(dd); _Ref(elem)
+    for vc in set.vcs
+        cube = _Cube(dd, vc.indices, vc.phases); _Ref(cube)
+        tmp = _AND(dd, elem, cube); _Ref(tmp)
+        _Deref(dd, cube); _Deref(dd, elem)
+        elem = tmp
+    end
+    # At this point elem is referenced
+    tmp = _OR(dd, set.Rroot[], elem); _Ref(tmp)
+    _Deref(dd, set.Rroot[]); _Deref(dd, elem)
+    set.Rroot[] = tmp
+    return set
+end
+
+# Returns true iff e can be represented with bits in phases
+function _phases_trunc_simple!(vc, e)
+    for r in 1:vc.nvars
+        vc.phases[r] = _bit(e)
+        e >>>= 1
+    end
+    return iszero(e)
+end
+
+Base.in(x::T, set::IntSet{T}) where {N,T<:Integer} = _in(x, set)
+Base.in(x, ::IntSet) = false
+Base.in(x::NTuple{N,T}, set::IntTupleSet{N,T}) where {N,T<:Integer} = _in(x, set)
+Base.in(x, ::IntTupleSet) = false
+function _eval_values_set(set)
+    for vc in set.vcs
+        set_values!(vc)
+    end
+    return _Eval(set.mng.dd, set.Rroot[], set.mng.values)
+end
+function _in(x, set)
+    _phases_trunc!(set, x) !== 0 && return false
+    # _phases_trunc updates the phases of the vc is set.vcs
+    return _eval_values_set(set)
+end
+
+Base.delete!(set::IntSet, x) = _delete!(set, x)
+Base.delete!(set::IntTupleSet, x) = _delete!(set, x)
+function _delete!(set, x)
+    x ∈ set || return set
+    # ∈ updates the phases of the vc is set.vcs
+    dd = set.mng.dd
+    elem = _One(dd); _Ref(elem)
+    for vc in set.vcs
+        cube = _Cube(dd, vc.indices, vc.phases); _Ref(cube)
+        tmp = _AND(dd, elem, cube); _Ref(tmp)
+        _Deref(dd, cube); _Deref(dd, elem)
+        elem = tmp
+    end
+    # At this point elem is referenced
+    not_elem = _NOT(dd, elem); _Ref(not_elem)
+    _Deref(dd, elem)
+    tmp = _AND(dd, set.Rroot[], not_elem); _Ref(tmp)
+    _Deref(dd, set.Rroot[]); _Deref(dd, not_elem)
+    set.Rroot[] = tmp
+    return set
+end
