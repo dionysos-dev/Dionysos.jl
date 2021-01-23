a544a8dfe41e6fae6b28f0d759f2ecfe59309677
Documenter.jl <documenter@juliadocs.github.io>
2021-01-23 13:42:07 +0000

    build based on c8b568c

---
+++ previews/PR76/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Dionysos","page":"Index","title":"Dionysos","text":"","category":"section"},{"location":"","page":"Index","title":"Index","text":"Dionysos implements a solver for the optimal control of cyber-physical systems.","category":"page"}]
+}
