ce50d76eb6897fa4ede3b88cddba0ad60c2e48d7
JulienCalbert <julien.calbert@uclouvain.be>
2021-02-10 11:05:32 +0100

    move pathplanning to litterate

--- src/Dionysos.jl
+++ src/Dionysos.jl
@@ -3,9 +3,11 @@ module Dionysos
 # TODO Reenable once https://github.com/sisl/CUDD.jl/issues/15#issuecomment-719958808 is resolved
 #include("BDD/BDD.jl")
 
+include("Abstraction/abstraction.jl")
 include("optimal_control.jl")
 include("bemporad_morari.jl")
 include("q_learning.jl")
 include("branch_and_bound.jl")
+include("plotting.jl")
 
 end # module
