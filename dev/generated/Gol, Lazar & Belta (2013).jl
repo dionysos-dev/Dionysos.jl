1458acfe5a452298a42abfc278c5422d5796014c
Documenter.jl <documenter@juliadocs.github.io>
2021-11-11 19:19:25 +0000

    build based on b829c94

--- dev/generated/Gol, Lazar & Belta (2013).jl
+++ dev/generated/Gol, Lazar & Belta (2013).jl
@@ -74,46 +74,56 @@ objective_value = MOI.get(optimizer, MOI.ObjectiveValue())
 
 xu = MOI.get(optimizer, ContinuousTrajectoryAttribute());
 
-using Plots
+using PyPlot
 using Colors
 
 ##Auxiliary function for annotating
-function text_in_set_plot!(pl, po, t; kws...)
+function text_in_set_plot!(ax, po, t;  fillcolor = :white, linecolor = :black, fillalpha = 1)
     ##solve finding center (other solvers? https://jump.dev/JuMP.jl/dev/installation/#Supported-solvers)
     solver = optimizer_with_attributes(GLPK.Optimizer, "presolve" => GLPK.ON)
-    plot!(pl, po; kws...)
+    poly = matplotlib.patches.Polygon([x for x in points(po)])
+    poly.set_facecolor(fillcolor)
+    poly.set_edgecolor(linecolor)
+    poly.set_alpha(fillalpha)
+    ax.add_patch(poly)
+
     if t !== nothing
         c, r = hchebyshevcenter(hrep(po), solver, verbose=0)
-        annotate!(pl, [(c..., text(t, 12))])
+        ax.annotate(t, c)
     end
 end
 
 ##Initialize our canvas
-p = Plots.plot(fmt = :png, fillcolor = :white)
+PyPlot.pygui(true)
+fig = PyPlot.figure()
+
+ax = PyPlot.axes(aspect = "equal")
+ax.set_xlim(-10.5,3)
+ax.set_ylim(-10.5,3)
 
 ##Show the discrete modes
 for mode in states(system)
     t = (system.ext[:q_T] in [mode, mode + 11]) ? "XT" : (mode == system.ext[:q_A] ? "A" : (mode == system.ext[:q_B] ? "B" :
             mode <= 11 ? string(mode) : string(mode - 11)))
-    text_in_set_plot!(p, stateset(system, mode), t, fillcolor = :white, linecolor = :black)
+    text_in_set_plot!(ax, stateset(system, mode), t, fillcolor = "none", linecolor = :black)
 end
 
 ##Plot obstacles
 for i in eachindex(system.ext[:obstacles])
-    text_in_set_plot!(p, system.ext[:obstacles][i], "O$i", fillcolor = :black, fillalpha = 0.1)
+    text_in_set_plot!(ax, system.ext[:obstacles][i], "O$i", fillcolor = :black, fillalpha = 0.1)
 end
 
 
 ##Initial state
-scatter!(p, [x0[1]], [x0[2]])
-annotate!(p, [(x0[1], x0[2] - 0.5, text("x0", 10))])
+ax.scatter([x0[1]], [x0[2]])
+ax.annotate("x0", [x0[1], x0[2]-0.5])
 
 ##Split the vector into x1 and x2
 x1 = [xu.x[j][1] for j in eachindex(xu.x)]
 x2 = [xu.x[j][2] for j in eachindex(xu.x)]
 
 ##Plot the trajectory
-scatter!(p, x1, x2)
+ax.scatter(x1, x2)
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
 
