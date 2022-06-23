623a83e171ed3fece99d4becdb4ef8b55441ec1a
vdebauche <vividebauche@hotmail.com>
2022-06-23 19:03:40 +0200

    creation of package BipedRobot

---
+++ deps/BipedRobot/test/runtest.jl
@@ -0,0 +1,13 @@
+using BipedRobot
+using RigidBodyDynamics
+using SymPy
+
+# Test for mechanism via URDF + setnominal :
+#mechanism_urdf = BipedRobot.mechanism(symbolic = false, add_contact_points = true, add_flat_ground = true)
+#state_urdf = MechanismState(mechanism_urdf)
+#BipedRobot.setnominal!(state_urdf)
+
+# Test for mechanism via symbolic variables :
+mechanism_sym = BipedRobot.mechanism(symbolic = true, add_contact_points = true, add_flat_ground = true)
+state_sym = MechanismState(mechanism_sym)
+simplify.(mass_matrix(state_sym))
