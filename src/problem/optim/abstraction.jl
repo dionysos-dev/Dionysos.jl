bb0b6ae37e93576e960584801ae3aee8efa1030c
Benoît Legat <benoit.legat@gmail.com>
2022-09-15 11:27:02 +0200

    Fixes

--- src/problem/optim/abstraction.jl
+++ src/problem/optim/abstraction.jl
@@ -4,16 +4,16 @@ module Abstraction
 
 using JuMP
 
-using ..Problem
-using ...Dionysos.Control
-using ...Dionysos.Domain
+import Dionysos
 
 mutable struct Optimizer{T} <: MOI.AbstractOptimizer
-    state_grid::Union{Nothing, Domain.Grid}
-    input_grid::Union{Nothing, Domain.Grid}
-    problem::Union{Nothing, Problem.OptimalControlProblem}
+    state_grid::Union{Nothing, Dionysos.Domain.Grid}
+    input_grid::Union{Nothing, Dionysos.Domain.Grid}
+    problem::Union{Nothing, Dionysos.Problem.OptimalControlProblem}
     function Optimizer{T}() where {T}
         return new{T}(
+            nothing,
+            nothing,
             nothing,
         )
     end
@@ -29,37 +29,47 @@ function MOI.get(model::Optimizer, param::MOI.RawOptimizerAttribute)
     getproperty(model, Symbol(param.name))
 end
 
-function build_abstraction(system, state_grid::Domain.Grid, input_grid::Domain.Grid)
-    Xfull = DO.DomainList(state_grid)
-    DO.add_set!(Xfull, system.X, DO.OUTER)
-    Ugrid = DO.Domain.GridFree(u0, h)
-    Ufull = DO.DomainList(input_grid)
-    DO.add_set!(Ufull, _U_, DO.OUTER)
-    symmodel = SY.NewSymbolicModelListList(Xfull, Ufull)
-    @time SY.compute_symmodel_from_controlsystem!(symmodel, contsys)
+function build_abstraction(
+    system,
+    state_grid::Dionysos.Domain.Grid,
+    input_grid::Dionysos.Domain.Grid,
+)
+    Xfull = Dionysos.Domain.DomainList(state_grid)
+    Dionysos.Domain.add_set!(Xfull, system.X, Dionysos.Domain.OUTER)
+    Ufull = Dionysos.Domain.DomainList(input_grid)
+    Dionysos.Domain.add_set!(Ufull, system.U, Dionysos.Domain.OUTER)
+    symmodel = Dionysos.Symbolic.NewSymbolicModelListList(Xfull, Ufull)
+    @time Dionysos.Symbolic.compute_symmodel_from_controlsystem!(
+        symmodel,
+        system,
+    )
     return symmodel
 end
 
-function build_abstraction(problem::Problem.OptimalControlProblem, state_grid::Domain.Grid, input_grid::Domain.Grid)
+function build_abstraction(
+    problem::Dionysos.Problem.OptimalControlProblem,
+    state_grid::Dionysos.Domain.Grid,
+    input_grid::Dionysos.Domain.Grid,
+)
     symmodel = build_abstraction(problem.system, state_grid, input_grid)
-    Xinit = DO.DomainList(state_grid)
-    DO.add_subset!(Xinit, symmodel.Xdom, problem.initial_set, DO.OUTER)
-    Xtarget = DO.DomainList(state_grid)
-    DO.add_subset!(Xinit, symmodel.Xdom, problem.target_set, DO.OUTER)
-    return Problem.OptimalControlProblem(
+    Xinit = Dionysos.Domain.DomainList(state_grid)
+    Dionysos.Domain.add_subset!(Xinit, symmodel.Xdom, problem.initial_set, Dionysos.Problem.OUTER)
+    Xtarget = Dionysos.Domain.DomainList(state_grid)
+    Dionysos.Domain.add_subset!(Xinit, symmodel.Xdom, problem.target_set, Dionysos.Problem.OUTER)
+    return Dionysos.Problem.OptimalControlProblem(
         symmodel,
         Xinit,
         Xtarget,
-        problem.state_cost, # TODO this is the continuous cost, not the abstraction
-        problem.transition_cost, # TODO this is the continuous cost, not the abstraction
-        problem.time, # TODO this is the continuous time, not the number of transition
+        problem.state_cost, # TODionysos.Problem this is the continuous cost, not the abstraction
+        problem.transition_cost, # TODionysos.Problem this is the continuous cost, not the abstraction
+        problem.time, # TODionysos.Problem this is the continuous time, not the number of transition
     )
 end
 
 function MOI.optimize!(optimizer::Optimizer{T}) where {T}
     discrete_problem = build_abstraction(optimizer.problem, optimizer.state_grid, optimizer.input_grid)
-    optimizer.controller = CO.NewControllerList()
-    @time CO.compute_controller_reach!(
+    optimizer.controller = Dionysos.Control.NewControllerList()
+    @time Dionysos.Control.compute_controller_reach!(
         optimizer.controller,
         discrete_problem.system.autom,
         discrete_problem.initial_set,
