d92ce89667ddf00313d362749987f713f8bdb09e
Documenter.jl <documenter@juliadocs.github.io>
2022-12-26 13:15:33 +0000

    build based on 7e70157

--- dev/generated/Path planning.jl
+++ dev/generated/Path planning.jl
@@ -1,6 +1,7 @@
 using StaticArrays
 
 using Dionysos
+using Dionysos.Problem
 const DI = Dionysos
 const UT = DI.Utils
 const DO = DI.Domain
@@ -16,54 +17,22 @@ F_sys = problem.system.f;
 _X_ = problem.system.X;
 _U_ = problem.system.U;
 
-ngrowthbound = 5;
-function L_growthbound(u)
-    β = abs(u[1]/cos(atan(tan(u[2])/2)))
-    return SMatrix{3,3}(
-        0.0, 0.0, 0.0,
-        0.0, 0.0, 0.0,
-        β, β, 0.0)
-end;
-
-sysnoise = SVector(0.0, 0.0, 0.0);
-measnoise = SVector(0.0, 0.0, 0.0);
-
-tstep = 0.3;
-nsys = 5;
-
-contsys = ST.NewControlSystemGrowthRK4(tstep, F_sys, L_growthbound, sysnoise,
-                                       measnoise, nsys, ngrowthbound);
-
 x0 = SVector(0.0, 0.0, 0.0);
 h = SVector(0.2, 0.2, 0.2);
-Xgrid = DO.GridFree(x0, h);
-
-Xfull = DO.DomainList(Xgrid);
-DO.add_set!(Xfull, _X_, DO.OUTER);
+state_grid = DO.GridFree(x0, h);
 
 u0 = SVector(0.0, 0.0);
 h = SVector(0.3, 0.3);
-Ugrid = DO.GridFree(u0, h);
+input_grid = DO.GridFree(u0, h);
 
-Ufull = DO.DomainList(Ugrid);
-DO.add_set!(Ufull, _U_, DO.OUTER);
+using JuMP
+optimizer = MOI.instantiate(Abstraction.Optimizer)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("problem"), problem)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("state_grid"), state_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("input_grid"), input_grid)
+MOI.optimize!(optimizer)
 
-symmodel = SY.NewSymbolicModelListList(Xfull, Ufull);
-@time SY.compute_symmodel_from_controlsystem!(symmodel, contsys)
-
-_I_ = problem.initial_set;
-_T_ = problem.target_set;
-
-Xinit = DO.DomainList(Xgrid);
-DO.add_subset!(Xinit, Xfull, _I_, DO.OUTER)
-initlist = [SY.get_state_by_xpos(symmodel, pos) for pos in DO.enum_pos(Xinit)];
-
-Xtarget = DO.DomainList(Xgrid)
-DO.add_subset!(Xtarget, Xfull, _T_, DO.OUTER)
-targetlist = [SY.get_state_by_xpos(symmodel, pos) for pos in DO.enum_pos(Xtarget)];
-
-contr = CO.NewControllerList();
-@time CO.compute_controller_reach!(contr, symmodel.autom, initlist, targetlist)
+controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("controller"));
 
 nstep = 100;
 x0 = SVector(0.4, 0.4, 0.0);
