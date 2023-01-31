efa0265d1013a13845156e881b954d567e9f2df0
Lucas Egidio <12188889+egidioln@users.noreply.github.com>
2023-01-31 14:30:59 +0100

    fixed _get_min_bounding_box and added more tests (#185)
    * fixed _get_min_bounding_box
    
    * tests added
    
    * sleeps deleted

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,6 +13,8 @@ include("./domain/test_general_domain.jl")
 include("./symbolic/test_automaton.jl")
 include("./symbolic/test_symbolicmodel.jl")
 include("./symbolic/test_lazy_symbolic.jl")
+include("./symbolic/test_ellipsoidal_transitions.jl")
+
 
 include("./control/test_fromcontrolsystemgrowth.jl")
 include("./control/test_fromcontrolsystemlinearized.jl")
