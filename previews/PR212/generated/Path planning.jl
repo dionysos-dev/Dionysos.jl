06118ca3841e30089d1792e724ff31e8df8cc1a9
Documenter.jl <documenter@juliadocs.github.io>
2023-05-20 15:35:17 +0000

    build based on eca5243

---
+++ previews/PR212/generated/Path planning.jl
@@ -0,0 +1,58 @@
+using StaticArrays, Plots
+
+using Dionysos
+using Dionysos.Problem
+const DI = Dionysos
+const UT = DI.Utils
+const DO = DI.Domain
+const ST = DI.System
+const SY = DI.Symbolic
+const CO = DI.Control
+
+include(joinpath(dirname(dirname(pathof(Dionysos))), "problems", "PathPlanning.jl"))
+
+problem = PathPlanning.problem();
+
+F_sys = problem.system.f;
+_X_ = problem.system.X;
+_U_ = problem.system.U;
+
+x0 = SVector(0.0, 0.0, 0.0);
+h = SVector(0.2, 0.2, 0.2);
+state_grid = DO.GridFree(x0, h);
+
+u0 = SVector(0.0, 0.0);
+h = SVector(0.3, 0.3);
+input_grid = DO.GridFree(u0, h);
+
+using JuMP
+optimizer = MOI.instantiate(Abstraction.Optimizer)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("problem"), problem)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("state_grid"), state_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("input_grid"), input_grid)
+MOI.optimize!(optimizer)
+
+abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("controller"))
+
+nstep = 100
+function reached(x)
+    if x∈problem.target_set
+        return true
+    else
+        return false
+    end
+end
+
+x0 = SVector(0.4, 0.4, 0.0)
+x_traj, u_traj = CO.get_closed_loop_trajectory(problem.system.f, controller, x0, nstep; stopping=reached)
+
+fig = plot(aspect_ratio=:equal)
+Plots.plot!(problem.system.X; dims=[1,2], color=:yellow, opacity=0.5)
+Plots.plot!(problem.initial_set; dims=[1,2], color=:green)
+Plots.plot!(problem.target_set; dims=[1,2], color=:red)
+UT.plot_traj!(x_traj; dims=[1,2])
+display(fig)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
