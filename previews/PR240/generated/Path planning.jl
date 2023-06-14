1527865d6a9927e36435a2f0cb86915bf8169243
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 17:24:44 +0000

    build based on 4099eec

---
+++ previews/PR240/generated/Path planning.jl
@@ -0,0 +1,77 @@
+using StaticArrays, Plots
+
+using Dionysos
+const DI = Dionysos
+const UT = DI.Utils
+const DO = DI.Domain
+const ST = DI.System
+const SY = DI.Symbolic
+const CO = DI.Control
+const PR = DI.Problem
+const OP = DI.Optim
+const AB = OP.Abstraction
+
+include(joinpath(dirname(dirname(pathof(Dionysos))), "problems", "PathPlanning.jl"))
+
+concrete_problem = PathPlanning.problem(; simple = true, approx_mode = "growth");
+concrete_system = concrete_problem.system;
+
+x0 = SVector(0.0, 0.0, 0.0);
+h = SVector(0.2, 0.2, 0.2);
+state_grid = DO.GridFree(x0, h);
+
+u0 = SVector(0.0, 0.0);
+h = SVector(0.3, 0.3);
+input_grid = DO.GridFree(u0, h);
+
+using JuMP
+optimizer = MOI.instantiate(AB.SCOTSAbstraction.Optimizer)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("concrete_problem"), concrete_problem)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("state_grid"), state_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("input_grid"), input_grid)
+MOI.optimize!(optimizer)
+
+abstract_system = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_system"))
+abstract_problem = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_problem"))
+abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+concrete_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_controller"))
+
+nstep = 100
+function reached(x)
+    if x ∈ concrete_problem.target_set
+        return true
+    else
+        return false
+    end
+end
+x0 = SVector(0.4, 0.4, 0.0)
+x_traj, u_traj = CO.get_closed_loop_trajectory(
+    concrete_system.f,
+    concrete_controller,
+    x0,
+    nstep;
+    stopping = reached,
+)
+
+fig = plot(; aspect_ratio = :equal);
+
+plot!(concrete_system.X; color = :yellow, opacity = 0.5);
+
+plot!(abstract_system.Xdom; color = :blue, opacity = 0.5);
+
+plot!(concrete_problem.initial_set; color = :green, opacity = 0.2);
+plot!(concrete_problem.target_set; dims = [1, 2], color = :red, opacity = 0.2);
+
+plot!(
+    SY.get_domain_from_symbols(abstract_system, abstract_problem.initial_set);
+    color = :green,
+);
+plot!(
+    SY.get_domain_from_symbols(abstract_system, abstract_problem.target_set);
+    color = :red,
+);
+
+plot!(UT.DrawTrajectory(x_traj); ms = 0.5)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
