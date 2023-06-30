3f1a48d7757000531d949ec4cae9ec9e52488de5
Documenter.jl <documenter@juliadocs.github.io>
2023-06-30 07:58:26 +0000

    build based on 8c8ab30

---
+++ previews/PR264/generated/Lazy-abstraction-reachability.jl
@@ -0,0 +1,169 @@
+using StaticArrays, JuMP, Plots
+
+using Dionysos
+const DI = Dionysos
+const UT = DI.Utils
+const DO = DI.Domain
+const ST = DI.System
+const SY = DI.Symbolic
+const CO = DI.Control
+const PR = DI.Problem
+const OP = DI.Optim
+const AB = OP.Abstraction
+
+include("../../../problems/simple_problem.jl")
+
+# specific functions
+function post_image(abstract_system, concrete_system, xpos, u)
+    Xdom = abstract_system.Xdom
+    x = DO.get_coord_by_pos(Xdom.grid, xpos)
+    Fx = concrete_system.f_eval(x, u)
+    r = Xdom.grid.h / 2.0 + concrete_system.measnoise
+    Fr = r
+
+    rectI = DO.get_pos_lims_outer(Xdom.grid, UT.HyperRectangle(Fx .- Fr, Fx .+ Fr))
+    ypos_iter = Iterators.product(DO._ranges(rectI)...)
+    over_approx = []
+    allin = true
+    for ypos in ypos_iter
+        ypos = DO.set_in_period_pos(Xdom, ypos)
+        if !(ypos in Xdom)
+            allin = false
+            break
+        end
+        target = SY.get_state_by_xpos(abstract_system, ypos)[1]
+        push!(over_approx, target)
+    end
+    return allin ? over_approx : []
+end
+
+function pre_image(abstract_system, concrete_system, xpos, u)
+    grid = abstract_system.Xdom.grid
+    x = DO.get_coord_by_pos(grid, xpos)
+    potential = Int[]
+    x_prev = concrete_system.f_backward(x, u)
+    xpos_cell = DO.get_pos_by_coord(grid, x_prev)
+    n = 2
+    for i in (-n):n
+        for j in (-n):n
+            x_n = (xpos_cell[1] + i, xpos_cell[2] + j)
+            x_n = DO.set_in_period_pos(abstract_system.Xdom, x_n)
+            if x_n in abstract_system.Xdom
+                cell = SY.get_state_by_xpos(abstract_system, x_n)[1]
+                if !(cell in potential)
+                    push!(potential, cell)
+                end
+            end
+        end
+    end
+    return potential
+end
+
+function compute_reachable_set(rect::UT.HyperRectangle, concrete_system, Udom)
+    r = (rect.ub - rect.lb) / 2.0 + concrete_system.measnoise
+    Fr = r
+    x = UT.get_center(rect)
+    n = UT.get_dims(rect)
+    lb = fill(Inf, n)
+    ub = fill(-Inf, n)
+    for upos in DO.enum_pos(Udom)
+        u = DO.get_coord_by_pos(Udom.grid, upos)
+        Fx = concrete_system.f_eval(x, u)
+        lb = min.(lb, Fx .- Fr)
+        ub = max.(ub, Fx .+ Fr)
+    end
+    lb = SVector{n}(lb)
+    ub = SVector{n}(ub)
+    return UT.HyperRectangle(lb, ub)
+end
+minimum_transition_cost(symmodel, contsys, source, target) = 1.0
+
+concrete_problem = SimpleProblem.problem()
+concrete_system = concrete_problem.system
+
+hx = [0.5, 0.5]
+u0 = SVector(0.0, 0.0)
+hu = SVector(0.5, 0.5)
+Ugrid = DO.GridFree(u0, hu)
+hx_heuristic = [1.0, 1.0] * 1.5
+maxIter = 100
+
+optimizer = MOI.instantiate(AB.LazyAbstraction.Optimizer)
+
+AB.LazyAbstraction.set_optimizer!(
+    optimizer,
+    concrete_problem,
+    maxIter,
+    pre_image,
+    post_image,
+    compute_reachable_set,
+    minimum_transition_cost,
+    hx_heuristic,
+    hx,
+    Ugrid,
+)
+
+using Suppressor
+@suppress begin # this is a workaround to supress the undesired output of SDPA
+    MOI.optimize!(optimizer)
+end
+
+abstract_system = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_system"))
+abstract_problem = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_problem"))
+abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+concrete_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_controller"))
+abstract_lyap_fun = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_lyap_fun"))
+concrete_lyap_fun = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_lyap_fun"));
+
+cost_eval(x, u) = UT.function_value(concrete_problem.transition_cost, x, u)
+reached(x) = x ∈ concrete_problem.target_set
+nstep = typeof(concrete_problem.time) == PR.Infinity ? 100 : concrete_problem.time; # max num of steps
+
+x0 = UT.get_center(concrete_problem.initial_set)
+x_traj, u_traj, cost_traj = CO.get_closed_loop_trajectory(
+    concrete_system.f_eval,
+    concrete_controller,
+    cost_eval,
+    x0,
+    nstep;
+    stopping = reached,
+    noise = false,
+)
+
+cost_bound = concrete_lyap_fun(x0)
+cost_true = sum(cost_traj);
+println("Goal set reached")
+println("Guaranteed cost:\t $(cost_bound)")
+println("True cost:\t\t $(cost_true)")
+
+fig = plot(; aspect_ratio = :equal);
+#We display the concrete domain
+plot!(concrete_system.X; color = :yellow, opacity = 0.5);
+
+#We display the abstract domain
+plot!(abstract_system.Xdom; color = :blue, opacity = 0.5);
+
+#We display the concrete specifications
+plot!(concrete_problem.initial_set; color = :green, opacity = 0.8);
+plot!(concrete_problem.target_set; dims = [1, 2], color = :red, opacity = 0.8);
+
+#We display the concrete trajectory
+plot!(UT.DrawTrajectory(x_traj); ms = 0.5)
+
+fig = plot(; aspect_ratio = :equal);
+plot!(abstract_system; dims = [1, 2], cost = true, lyap_fun = optimizer.lyap_fun)
+
+fig = plot(; aspect_ratio = :equal)
+plot!(
+    optimizer.abstract_system_heuristic;
+    arrowsB = false,
+    dims = [1, 2],
+    cost = true,
+    lyap_fun = optimizer.bell_fun,
+)
+
+fig = plot(; aspect_ratio = :equal)
+plot!(optimizer.lazy_search_problem)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
