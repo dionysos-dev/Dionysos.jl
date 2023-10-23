e145251afa707c99dd15cc7bcaf8666d9f5ca1d2
Alexis Vuille <alexis.vuille@uclouvain.be>
2023-10-23 16:42:25 +0200

    test_newton_bisection_methods

---
+++ test/utils/optim/bisection.jl
@@ -0,0 +1,23 @@
+using Test
+using Dionysos
+bisection = Dionysos.Utils.bisection
+
+# Define the test function, its derivative, and second derivative
+@testset "Bissection" begin
+    # Define a test function
+    test_function(x) = x^2 - 4
+    ϵ = 1e-6
+
+    # Call the bisection function
+    result = bisection(test_function, interval=[0, 4], δ=ϵ, verbose=false)
+    @test isapprox(result[1], -4.0, atol=ϵ) && 0 ≤ result[2] ≤ 4
+
+    result = bisection(test_function, interval=[0, 4], δ=ϵ, verbose=true)
+    @test isapprox(result[1], -4.0, atol=ϵ) && 0 ≤ result[2] ≤ 4
+    
+    result = bisection(test_function, interval=[1, 3], δ=ϵ, verbose=true)
+    @test isapprox(result[1], -3.0, atol=ϵ) && 0 ≤ result[2] ≤ 4
+
+    result = bisection(test_function,interval = [-5,4],δ=ϵ,verbose=false,stopIfNegative=true)
+    @test (-5 ≤ result[2] ≤ 4) && abs(result[1]+4.0) > ϵ && result[1] < 0
+end
