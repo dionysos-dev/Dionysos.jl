56340dbf904415051135eeaacb2ca96ef7140d8e
DeligneS <sim.deligne@gmail.com>
2024-04-23 13:27:07 +0200

    Fix urdf path issues

--- problems/utils/model_urdf.jl
+++ problems/utils/model_urdf.jl
@@ -3,10 +3,9 @@ using RigidBodyDynamics
 function get_mechanism(; wanted_mech = "double_pendulum")
     # Load the URDF file
     if wanted_mech == "double_pendulum"
-        urdf_path = joinpath("..", "utils", "urdfs", "double_pendulum_right", "robot.urdf")
+        urdf_path = joinpath(@__DIR__, "urdfs", "double_pendulum_right", "robot.urdf")
     else
-        # urdf_path = joinpath("..", "utils", "urdfs", "doublependulum_new_knee_fixed.urdf")
-        urdf_path = joinpath("..", "utils", "urdfs", "single_pendulum_hip", "robot.urdf")
+        urdf_path = joinpath(@__DIR__, "urdfs", "single_pendulum_hip", "robot.urdf")
     end
     mechanism = parse_urdf(Float64, urdf_path)
     remove_fixed_tree_joints!(mechanism)
