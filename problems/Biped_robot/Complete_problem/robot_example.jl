daed5438a3f915dd5d9d04553c9df6ff03c9bf74
GaXirs <151014420+GaXirs@users.noreply.github.com>
2025-03-18 10:07:05 +0100

    First draft problem statement

--- problems/Biped_robot/Complete_problem/robot_example.jl
+++ problems/Biped_robot/Complete_problem/robot_example.jl
@@ -24,7 +24,7 @@ include(
 ################### File Parameters ###################
 #######################################################
 filename_save = joinpath(@__DIR__, "Abstraction_solver.jld2")
-do_empty_optim = true
+do_empty_optim = false
 verify_save = true
 
 #######################################################
@@ -41,7 +41,7 @@ input_space = MathematicalSystems.inputset(concrete_system)
 println("n_state: ", n_state)
 println("n_input: ", n_input)
 
-x0 = SVector{n_state, Float64}(zeros(n_state))
+x0 = SVector{n_state, Float64}(ones(n_state).*(0))
 hx = SVector{n_state, Float64}(fill(1.0, n_state)) # Intentional big discretization step (otherwise way too many values and infinite optimize!)
 state_grid = DO.GridFree(x0, hx)
 
@@ -66,6 +66,9 @@ MOI.set(optimizer, MOI.RawOptimizerAttribute("print_level"), 2)
 
 ### Optimize
 if(do_empty_optim)
+    #######################################################
+    ################# Empty Optimisations #################
+    #######################################################
     MOI.optimize!(optimizer)
 
     # Save the abstraction solver
@@ -100,7 +103,59 @@ if(do_empty_optim)
         end
     end
 else
+    # Reload the result
     file = jldopen(filename_save, "r")
     reloaded_solver = file["my_abstraction_solver"]
     MOI.set(optimizer, MOI.RawOptimizerAttribute("abstraction_solver"), reloaded_solver)
+
+    #######################################################
+    ################# Problem definition ##################
+    #######################################################
+    _I_ = UT.HyperRectangle(x0, x0) # We force the system to start in the cell in which x_0 is
+    _T_ = UT.HyperRectangle([fill(-π/4, 2)..., fill(0, 2)..., fill(1,2)..., fill(-2,2)...], 
+        [fill(π/4, 2)..., fill(π/2, 2)..., fill(2,2)..., fill(-1,2)...])
+      
+    concrete_problem = Dionysos.Problem.OptimalControlProblem(
+        concrete_system,
+        _I_,
+        _T_,
+        nothing,
+        nothing,
+        Dionysos.Problem.Infinity(),
+    )
+    MOI.set(
+        optimizer,
+        MOI.RawOptimizerAttribute("concrete_problem"),
+        concrete_problem,
+    )
+    MOI.set(optimizer, MOI.RawOptimizerAttribute("early_stop"), false)
+    MOI.optimize!(optimizer)
+    abstract_problem_time =
+        MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_problem_time_sec"))
+    println("Time to solve the abstract problem: $(abstract_problem_time)")
+    
+    abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+    concrete_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_controller"))
+    controllable_set = MOI.get(optimizer, MOI.RawOptimizerAttribute("controllable_set"))
+    uncontrollable_set = MOI.get(optimizer, MOI.RawOptimizerAttribute("uncontrollable_set"))
+    
+    nstep = 300
+    function reached(x)
+        if x ∈ concrete_problem.target_set
+            return true
+        else
+            return false
+        end
+    end
+    
+    control_trajectory = ST.get_closed_loop_trajectory(
+        MOI.get(optimizer, MOI.RawOptimizerAttribute("discrete_time_system")),
+        concrete_controller,
+        x0,
+        nstep;
+        stopping = reached,
+    );
 end
+
+
+
