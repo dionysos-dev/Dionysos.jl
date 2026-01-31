5494518d4ac44ec459c06338953a446659f0e447
JulienCalbert <julien.calbert@uclouvain.be>
2026-01-31 15:36:38 +0100

    format

--- controller_runtime/scripts/start_lvserver.jl
+++ controller_runtime/scripts/start_lvserver.jl
@@ -9,5 +9,6 @@ using .LVServerTransport
 # )
 
 LVServerTransport.run_lvserver(; initOK = true)
-# LVServerTransport.run_json_server(endpoint="tcp://*:5556", verbose=true) # {"cmd":"load_controller","args":{"path":"C:/path/to/controller.file"}}, {"cmd":"step_controller","args":{"x":[0.1,0.2,0.3,0.4],"t":0.01}}
+# LVServerTransport.run_json_server(endpoint="tcp://*:5556", verbose=true) 
+# {"cmd":"load_controller","args":{"path":"C:/path/to/controller.file"}}, {"cmd":"step_controller","args":{"x":[0.1,0.2,0.3,0.4],"t":0.01}}
 # LVServerTransport.run_both(initOK=true, json_endpoint="tcp://*:5556", verbose=true)
