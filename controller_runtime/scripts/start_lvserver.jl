dbab4951d4709f220d1ecde083cd8a6517e2b43e
JulienCalbert <julien.calbert@uclouvain.be>
2026-01-31 15:20:41 +0100

    wrap output

--- controller_runtime/scripts/start_lvserver.jl
+++ controller_runtime/scripts/start_lvserver.jl
@@ -4,8 +4,11 @@ include(joinpath(@__DIR__, "..", "transports", "lvserver_transport.jl"))
 using .ControllerRuntime
 using .LVServerTransport
 
-ControllerRuntime.Server.ControllerServer.load_controller!(;
-    path = joinpath(@__DIR__, "..", "pid_cfg.jld2"),
-)
+# ControllerRuntime.Server.ControllerServer.load_controller!(;
+#     path = joinpath(@__DIR__, "..", "pid_cfg.jld2"),
+# )
 
-LVServerTransport.start_lvserver(; initOK = true)
+LVServerTransport.run_lvserver(; initOK = true)
+# LVServerTransport.run_json_server(endpoint="tcp://*:5556", verbose=true) # {"cmd":"load_controller","args":{"path":"C:/path/to/controller.file"}}, {"cmd":"step_controller","args":{"x":[0.1,0.2,0.3,0.4],"t":0.01}}
+# LVServerTransport.run_both(initOK=true, json_endpoint="tcp://*:5556", verbose=true)
+
