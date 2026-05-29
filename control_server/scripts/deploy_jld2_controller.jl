b16d870c8cd798f699e083597eac884c73be085d
Riccardo Bertollo <73940470+riccardobertollo@users.noreply.github.com>
2026-05-29 14:42:08 +0200

    Robot tests rb (#564)
    * Added script to generate MIMO PID
    
    * Adapted deploy script
    
    * Debugging LV-Julia communication
    
    * PID tuning
    
    * format
    
    ---------
    
    Co-authored-by: JulienCalbert <julien.calbert@uclouvain.be>

--- control_server/scripts/deploy_jld2_controller.jl
+++ control_server/scripts/deploy_jld2_controller.jl
@@ -38,7 +38,7 @@ result = SR.start_control_server(
     server_controller;
     port = port,
     log_data = true,
-    received_data_size = 2,
+    received_data_size = 8,
     state_to_vector = nothing,
 )
 
@@ -49,9 +49,26 @@ if result !== nothing
     t, measurements, controls, states = result
     println("Server session finished.")
     println("Logged $(length(t)) packets.")
+    println("Average dt: $((t[length(t)]-t[1])/length(t))")
 end
 
 using Plots
 
-plot(t, measurements[1, :]; label = "y")
-plot!(t, controls[1, :]; label = "u")
+p = plot(; layout = (2, 2), size = (1200, 900))
+
+for i in 1:4
+    plot!(p[i], t, measurements[i, :]; label = "Angle $i")
+
+    #= plot!(
+        p[i],
+        t,
+        measurements[i+4, :],
+        label = "Velocity $i"
+    ) =#
+
+    plot!(p[i], t, controls[i, :]; label = "Control input $i")
+
+    xlabel!(p[i], "Time")
+end
+
+display(p)
