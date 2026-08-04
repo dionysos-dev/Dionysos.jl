8bfc0873648ec8c65e1085f1f480564249106f50
Documenter.jl <documenter@juliadocs.github.io>
2026-08-04 09:46:23 +0000

    build based on 2842240

---
+++ previews/PR586/generated/DC-DC converter.jl
@@ -0,0 +1,133 @@
+using StaticArrays, JuMP, Plots
+
+using Dionysos
+const DI = Dionysos
+const UT = DI.Utils
+const ST = DI.System
+const MP = DI.Mapping
+const SY = DI.Symbolic
+const OP = DI.Optim
+const AB = OP.Abstraction
+
+include(
+    joinpath(dirname(dirname(pathof(Dionysos))), "problems", "DCDC", "dcdc_converter.jl"),
+);
+
+concrete_problem = DCDC.problem()
+concrete_system = concrete_problem.system
+
+x0 = SVector(0.0, 0.0)
+hx = SVector(2.0 / 4.0e3, 2.0 / 4.0e3)
+state_grid = MP.GridFree(x0, hx)
+u0 = SVector(1)
+hu = SVector(1)
+input_grid = MP.GridFree(u0, hu)
+
+optimizer = MOI.instantiate(AB.UniformGridAbstraction.Optimizer)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("concrete_problem"), concrete_problem)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("state_grid"), state_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("input_grid"), input_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("jacobian_bound"), DCDC.jacobian_bound())
+MOI.set(optimizer, MOI.RawOptimizerAttribute("time_step"), 0.5)
+MOI.set(
+    optimizer,
+    MOI.RawOptimizerAttribute("approx_mode"),
+    AB.UniformGridAbstraction.GROWTH,
+)
+
+MOI.optimize!(optimizer)
+
+abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+concrete_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_controller"))
+abstraction_time =
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("abstraction_construction_time_sec"))
+println("Time to construct the abstraction: $(abstraction_time)")
+abstract_problem_time =
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_problem_time_sec"))
+println("Time to solve the abstract problem: $(abstract_problem_time)")
+total_time = MOI.get(optimizer, MOI.RawOptimizerAttribute("solve_time_sec"))
+println("Total time: $(total_time)")
+
+invariant_set = MOI.get(optimizer, MOI.RawOptimizerAttribute("invariant_set"))
+invariant_set_complement =
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("invariant_set_complement"));
+
+nstep = 300
+x0 = SVector(1.2, 5.6)
+traj = ST.get_closed_loop_trajectory(
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("discrete_time_system")),
+    concrete_controller,
+    x0,
+    nstep,
+);
+
+fig = plot(; aspect_ratio = :equal);
+plot!(concrete_system.X; label = "", color = :grey);
+plot!(concrete_problem.initial_set; color = :green, label = "");
+plot!(traj; arrows = false, ms = 2.0, color = :blue)
+
+include(
+    joinpath(dirname(dirname(pathof(Dionysos))), "problems", "DCDC", "dcdc_converter.jl"),
+);
+
+concrete_problem = DCDC.problem()
+concrete_system = concrete_problem.system
+
+origin = SVector(0.0, 0.0)
+η = (2 / 4.0) * 10^(-3);
+
+ϵ = 0.1 * 0.01
+P = SMatrix{2, 2}(1.0224, 0.0084, 0.0084, 1.0031)
+state_grid = MP.GridEllipsoidalRectangular(origin, SVector(η, η), P / ϵ)
+
+u0 = SVector(1)
+hu = SVector(1)
+input_grid = MP.GridFree(u0, hu)
+
+optimizer = MOI.instantiate(AB.UniformGridAbstraction.Optimizer)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("concrete_problem"), concrete_problem)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("state_grid"), state_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("input_grid"), input_grid)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("jacobian_bound"), DCDC.jacobian_bound())
+MOI.set(
+    optimizer,
+    MOI.RawOptimizerAttribute("approx_mode"),
+    AB.UniformGridAbstraction.CENTER_SIMULATION,
+)
+MOI.set(optimizer, MOI.RawOptimizerAttribute("time_step"), 0.5)
+MOI.optimize!(optimizer);
+
+abstract_system = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_system"))
+abstract_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_controller"))
+concrete_controller = MOI.get(optimizer, MOI.RawOptimizerAttribute("concrete_controller"))
+abstraction_time =
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("abstraction_construction_time_sec"))
+println("Time to construct the abstraction: $(abstraction_time)")
+abstract_problem_time =
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("abstract_problem_time_sec"))
+println("Time to solve the abstract problem: $(abstract_problem_time)")
+total_time = MOI.get(optimizer, MOI.RawOptimizerAttribute("solve_time_sec"))
+println("Total time: $(total_time)")
+
+nstep = 300
+x0 = SVector(1.2, 5.6)
+traj = ST.get_closed_loop_trajectory(
+    MOI.get(optimizer, MOI.RawOptimizerAttribute("discrete_time_system")),
+    concrete_controller,
+    x0,
+    nstep,
+)
+
+Xmapping = SY.get_state_mapping(abstract_system)
+
+fig = plot(; aspect_ratio = :equal);
+plot!(concrete_system.X; label = "", color = :grey);
+plot!(
+    (invariant_set_complement, Xmapping);
+    color = :black,
+    label = "Invariant set complement",
+)
+plot!(concrete_problem.initial_set; color = :green, label = "");
+plot!(traj; arrows = false, ms = 2.0, color = :blue)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
