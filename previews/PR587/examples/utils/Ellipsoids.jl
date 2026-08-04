a86702f857de6d2cba3f72c685e4c12f1bdbb5f0
Documenter.jl <documenter@juliadocs.github.io>
2026-08-04 11:20:31 +0000

    build based on 11515db

---
+++ previews/PR587/examples/utils/Ellipsoids.jl
@@ -0,0 +1,102 @@
+# # Ellipsoids
+#
+# In this file, we will visit the basic features provided by Dionysos for manipulating ellipsoids:
+# - inclusion
+# - intersection
+# 
+# First, let us import a few packages that are necessary to run this example.
+using Dionysos
+using StaticArrays
+using LinearAlgebra
+import LazySets
+using Plots
+
+# The main package [Dionysos](https://github.com/dionysos-dev/Dionysos.jl) provides most important data structures that we will need.
+
+const DI = Dionysos
+const UT = DI.Utils
+
+# We define a plotting functions
+function plot_config!(fig, El0, El, Elnew)
+    if UT.is_included(Elnew, El0)
+        plot!(fig, El0; color = :red, label = "El0")
+        plot!(fig, Elnew; color = :green, label = "Elnew")
+    else
+        plot!(fig, Elnew; color = :green, label = "Elnew")
+        plot!(fig, El0; color = :red, label = "El0")
+    end
+    return plot!(fig, El; color = :blue, label = "El", show = true)
+end
+
+function analyze(fig1, fig2, i)
+    El0 = E0L[i]
+    Elnew = UT.scale_for_inclusion_contact_point(El0, El)
+    plot_config!(fig1, El0, El, Elnew)
+    println(UT.is_included(El0, El) ? "El0 ⊆ El" : "El0 ⊈ El")
+
+    Elnew = UT.scale_for_noninclusion_contact_point(El0, El)
+    plot_config!(fig2, El0, El, Elnew)
+    return println(!UT.is_disjoint(El0, El) ? "El0 ∩ El ≠ ∅" : "El0 ∩ El = ∅")
+end
+
+# We define some ellipsoids. `LazySets.Ellipsoid(c, Q)` is the set
+# `{x : (x−c)ᵀ Q⁻¹ (x−c) ≤ 1}` with shape matrix `Q`; to build it from the
+# quadratic-form matrix `P` of `{x : (x−c)ᵀ P (x−c) ≤ 1}`, pass `Q = inv(P)`.
+c = [1.5; 1.5]
+P = [
+    4.0 0.5
+    0.5 6.0
+]
+El = LazySets.Ellipsoid(c, inv(P))
+
+P0 = [
+    0.4 -0.1
+    -0.1 0.5
+]
+Q0 = inv(P0)
+vals = [4.1, 3.32, 2.8, 2.4]
+E0L = [LazySets.Ellipsoid([c0x; c0x - 0.2], Q0) for c0x in vals]
+
+# ## Case 1: non intersection
+fig1_1 = plot(; aspect_ratio = :equal);
+fig1_2 = plot(; aspect_ratio = :equal);
+analyze(fig1_1, fig1_2, 1)
+
+#
+plot!(fig1_1)
+
+#
+plot!(fig1_2)
+
+# ## Case 2: non intersection
+fig2_1 = plot(; aspect_ratio = :equal);
+fig2_2 = plot(; aspect_ratio = :equal);
+analyze(fig2_1, fig2_2, 2)
+
+#
+plot!(fig2_1)
+
+#
+plot!(fig2_2)
+
+# ## Case 3: intersection, non inclusion
+fig3_1 = plot(; aspect_ratio = :equal);
+fig3_2 = plot(; aspect_ratio = :equal);
+analyze(fig3_1, fig3_2, 3)
+
+#
+plot!(fig3_1)
+
+#
+plot!(fig3_2)
+
+# ## Case 4: inclusion
+fig4_1 = plot(; aspect_ratio = :equal);
+fig4_2 = plot(; aspect_ratio = :equal);
+analyze(fig4_1, fig4_2, 4)
+
+#
+plot!(fig4_1)
+
+#
+plot!(fig4_2)
